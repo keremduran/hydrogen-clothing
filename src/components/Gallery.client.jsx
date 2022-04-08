c7103e3d620b9c5242fabe63fc43b711ae428fbc
keremduran <keremduran@live.com>
2022-04-08 14:31:06 -0400

    Gallery code shortened

--- src/components/Gallery.client.jsx
+++ src/components/Gallery.client.jsx
@@ -1,75 +1,60 @@
-import {useProduct, MediaFile, Image} from '@shopify/hydrogen/client';
+import {useProduct, MediaFile} from '@shopify/hydrogen/client';
 import {useState} from 'react';
 
 const galleryItemDefaultClasses =
-  'w-[80vw] h-full snap-start md:h-auto object-cover object-center transition ease-in-out rounded-xl duration-700 delay-150 hover:-translate-y-1 hover:scale-[110%]';
-const smallItemClasses = `${galleryItemDefaultClasses} md:w-auto hover:opacity-90 cursor-pointer shadow-inner hover:shadow-xl flex-shrink-0 md:col-span-2 lg:col-span-1`;
-const largeItemClasses = `${galleryItemDefaultClasses} md:w-full flex-shrink-0 md:flex-shrink-none md:col-start-1 md:col-end-[-1] md:row-start-1 md:row-end-2 `;
-const galleryClassesArray = [
-  largeItemClasses,
-  smallItemClasses,
-  smallItemClasses,
-  smallItemClasses,
-  smallItemClasses,
-];
+  'w-[80vw] h-full rounded-xl snap-start md:h-auto object-cover object-center transition ease-in-out duration-300 delay-150 hover:-translate-y-4 hover:shadow-xl';
+const largeItemClasses = `${galleryItemDefaultClasses} md:col-span-full`;
+const smallItemClasses = `${galleryItemDefaultClasses} hover:opacity-90 cursor-pointer flex-shrink-0 md:col-span-2 lg:col-span-1`;
+
 /**
  * A client component that defines a media gallery for hosting images, 3D models, and videos of products
  */
 export default function Gallery() {
-  const {media, selectedVariant} = useProduct();
-  const [activeGalleryItem, setActiveGalleryItem] = useState(0);
-  const featuredMedia = selectedVariant.image || media[0]?.image;
-  const featuredMediaSrc = featuredMedia?.url.split('?')[0];
-  function handleGalleryItemClick(index) {
-    if (activeGalleryItem === index) {
-      return;
-    }
-    for (let i = 0; i < galleryClassesArray.length; i++) {
-      galleryClassesArray[i] = smallItemClasses;
-    }
+  const {media} = useProduct();
+  const [activeGalleryItemIndex, setActiveGalleryItemIndex] = useState(0);
 
-    galleryClassesArray[index] = largeItemClasses;
-    setActiveGalleryItem(index);
-  }
-  const galleryMedia = media.filter(
-    (med) =>
-      med.mediaContentType === MODEL_3D_TYPE ||
-      med.mediaContentType === VIDEO_TYPE ||
-      med.mediaContentType === EXTERNAL_VIDEO_TYPE ||
-      !med.image.url.includes(featuredMediaSrc),
-  );
   if (!media.length) {
     return null;
   }
 
+  let galleryMedia = media.filter(
+    (med) =>
+      med.mediaContentType === MODEL_3D_TYPE ||
+      med.mediaContentType === VIDEO_TYPE ||
+      med.mediaContentType === EXTERNAL_VIDEO_TYPE ||
+      med.mediaContentType === IMAGE_TYPE,
+  );
+  const galleryMediaIndexes = Array.from(
+    Array(galleryMedia.length),
+    (_, i) => (i + activeGalleryItemIndex) % galleryMedia.length,
+  );
+
   return (
     <div
-      className="gap-4 flex md:grid md:grid-cols-4 overflow-x-scroll no-scrollbar scroll-snap-x scroll-smooth h-[485px] md:h-auto place-content-start"
+      className={`gap-4 flex md:grid md:grid-cols-4 overflow-x-scroll no-scrollbar scroll-snap-x scroll-smooth h-[485px] md:h-auto place-content-start`}
       tabIndex="-1"
     >
-      <Image
-        data={selectedVariant.image}
-        onClick={() => handleGalleryItemClick(0)}
-        className={galleryClassesArray[0]}
-      />
-      {galleryMedia.map((med, index) => {
+      {galleryMediaIndexes.map((medIndex, index) => {
         let extraProps = {};
-
+        let med = galleryMedia[medIndex];
         if (med.mediaContentType === MODEL_3D_TYPE) {
           extraProps = MODEL_3D_PROPS;
         }
-
         return (
           <MediaFile
             tabIndex="0"
             key={med.id || med.image.id}
-            className={galleryClassesArray[index + 1]}
+            className={
+              medIndex === activeGalleryItemIndex
+                ? largeItemClasses
+                : smallItemClasses
+            }
             data={med}
             options={{
               height: '485',
               crop: 'center',
             }}
-            onClick={() => handleGalleryItemClick(index + 1)}
+            onClick={() => setActiveGalleryItemIndex(medIndex)}
             {...extraProps}
           />
         );
@@ -84,3 +69,4 @@ const MODEL_3D_PROPS = {
 };
 const VIDEO_TYPE = 'VIDEO';
 const EXTERNAL_VIDEO_TYPE = 'EXTERNAL_VIDEO';
+const IMAGE_TYPE = 'IMAGE';
